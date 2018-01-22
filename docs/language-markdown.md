74e43cea5b2b205fb48063df48e6dc58a86c3190
Glavin Wiechert <glavin.wiechert@gmail.com>
2018-01-22 01:07:16 -0400

    Add options table to Language docs

--- docs/language-markdown.md
+++ docs/language-markdown.md
@@ -2,5 +2,21 @@
 id: language-markdown
 title: Markdown
 ---
-## Beautifiers
+## Supported Beautifiers
 - [Prettier](/docs/beautifier-prettier.html)
+## Options
+| Option | [Prettier](/docs/beautifier-prettier.html) |
+| --- | --- |
+| [Arrow Parens](/docs/option-arrow-parens.html) | &#9989; |
+| [Convert Quotes](/docs/option-convert-quotes.html) | &#9989; |
+| [End With Comma](/docs/option-end-with-comma.html) | &#9989; |
+| [End With Semicolon](/docs/option-end-with-semicolon.html) | &#9989; |
+| [Indent Char](/docs/option-indent-char.html) | &#9989; |
+| [Indent Size](/docs/option-indent-size.html) | &#9989; |
+| [Indent With Tabs](/docs/option-indent-with-tabs.html) | &#9989; |
+| [Jsx Brackets](/docs/option-jsx-brackets.html) | &#9989; |
+| [Object Curly Spacing](/docs/option-object-curly-spacing.html) | &#9989; |
+| [Pragma Insert](/docs/option-pragma-insert.html) | &#9989; |
+| [Pragma Require](/docs/option-pragma-require.html) | &#9989; |
+| [Wrap Line Length](/docs/option-wrap-line-length.html) | &#9989; |
+| [Wrap Prose](/docs/option-wrap-prose.html) | &#9989; |
--- scripts/generate-docs/LanguageDoc.ts
+++ scripts/generate-docs/LanguageDoc.ts
@@ -1,12 +1,20 @@
-import { Language, Beautifier } from "unibeautify";
+import Unibeautify, {
+  Language,
+  Beautifier,
+  OptionsRegistry
+} from "unibeautify";
+import * as _ from "lodash";
 
-import { slugify } from "./utils";
+import { slugify, optionKeys, linkForBeautifier, linkForOption } from "./utils";
 import Doc from "./Doc";
 import MarkdownBuilder from "./MarkdownBuilder";
+import OptionsListDoc from "./OptionsListDoc";
 
 export default class LanguageDoc extends Doc {
+  private readonly optionsLookup: OptionsLookup;
   constructor(private language: Language, private beautifiers: Beautifier[]) {
     super();
+    this.optionsLookup = this.createOptionsLookup();
   }
 
   public get prefix(): string {
@@ -22,14 +30,92 @@ export default class LanguageDoc extends Doc {
   }
 
   protected get body(): string {
-    return new MarkdownBuilder()
-      .header("Beautifiers", 2)
-      .list(this.beautifiers.map(this.linkForBeautifier))
-      .build();
+    const builder = new MarkdownBuilder();
+    builder
+      .header("Supported Beautifiers", 2)
+      .list(this.beautifiers.map(this.linkForBeautifier));
+    builder.header("Options", 2);
+    this.appendOptionsTable(builder);
+    return builder.build();
   }
 
   private linkForBeautifier = (beautifier: Beautifier): string => {
     const docId = `beautifier-${slugify(beautifier.name)}`;
     return MarkdownBuilder.createDocLink(beautifier.name, docId);
   };
+
+  private appendOptionsTable(builder: MarkdownBuilder): MarkdownBuilder {
+    builder.append(
+      "| Option |" +
+        this.beautifiers
+          .map(beautifier => ` ${linkForBeautifier(beautifier)} |`)
+          .join("")
+    );
+    builder.append(
+      "| --- |" + this.beautifiers.map(beautifier => ` --- |`).join("")
+    );
+    const symbols = {
+      x: "&#10060;",
+      checkmark: "&#9989;"
+    };
+    Object.keys(this.allOptions).forEach(optionKey => {
+      const option = this.allOptions[optionKey];
+      let row = `| ${linkForOption(optionKey, option)} |`;
+      let isSupported = false;
+      this.beautifiers.forEach(beautifier => {
+        const beautifierSupportsOption: boolean = _.get(
+          this.optionsLookup as any,
+          `${beautifier.name}.${optionKey}`,
+          false
+        );
+        const symbol = beautifierSupportsOption ? symbols.checkmark : symbols.x;
+        row += ` ${symbol} |`;
+        if (beautifierSupportsOption) {
+          isSupported = true;
+        }
+      });
+      if (isSupported) {
+        builder.append(row);
+      }
+    });
+    return builder;
+  }
+
+  private createOptionsLookup(): OptionsLookup {
+    return this.beautifiers
+      .map(beautifier => ({
+        beautifier,
+        options: this.options(beautifier)
+      }))
+      .reduce(
+        (lookup, { beautifier, options }) => ({
+          ...lookup,
+          [beautifier.name]: options
+        }),
+        {}
+      );
+  }
+
+  private options(beautifier: Beautifier): OptionsRegistry {
+    const keys = optionKeys(beautifier, this.language);
+    const allOptions = this.allOptions;
+    return keys.reduce((options, key) => {
+      const option = allOptions[key];
+      if (!option) {
+        return options;
+      }
+      return {
+        ...options,
+        [key]: option
+      };
+    }, {});
+  }
+
+  private get allOptions(): OptionsRegistry {
+    return (Unibeautify as any).options;
+  }
+}
+
+interface OptionsLookup {
+  [beautifierName: string]: OptionsRegistry;
 }
